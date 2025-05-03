42ab70e945554bb3a3f6e62cbf7283b613633b8b
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-05-03 08:20:31 -0500

    agregando codigo para que se ejecute el html y script en el servidor

--- tareaAjax/server1.js
+++ tareaAjax/server1.js
@@ -1,6 +1,13 @@
 const express = require ("express");
 const app=express();
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
+  app.get('/script.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'script.js'));
+  });
+
 app.get('/data', (req, res) =>{
     fs.readFile('data.json', 'utf8', (err, data) => {
         if (err) return res.status(500).send('Error leyendo archivo');
