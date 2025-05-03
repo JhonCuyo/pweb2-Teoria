6bb9c7794276ed328ee6597663b660f0d4babb86
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-05-02 20:00:54 -0500

    leer los datos JSON en el servidor nodejs

--- tareaAjax/server1.js
+++ tareaAjax/server1.js
@@ -1,6 +1,13 @@
 const express = require ("express");
 const app=express();
 
+app.get('/data', (req, res) =>{
+    fs.readFile('data.json', 'utf8', (err, data) => {
+        if (err) return res.status(500).send('Error leyendo archivo');
+        res.json(JSON.parse(data));
+    });
+});
+
 app.listen(3000, ()=> {
     console.log("escuchando en puerto ", 3000);
     
