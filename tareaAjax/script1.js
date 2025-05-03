b85c252ee9d0d7f23beba10454e9f3b7d7f73802
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-05-02 20:51:25 -0500

    actualizando la funcion para que muestre los datos en la tabla

--- tareaAjax/script1.js
+++ tareaAjax/script1.js
@@ -4,7 +4,7 @@ function getSeleccionRegiones() {
     checkboxes.forEach(cb => regiones.push(cb.value));
     return regiones;
   }
-  function mostrarTabla(){
+function mostrarTabla(){
     fetch('/data')
     .then(response => response.json())
     .then(data => {
@@ -12,16 +12,25 @@ function getSeleccionRegiones() {
     const conteo = {};
 
     regionSeleccionada.forEach(region => {
-        conteo[region] = data.filter(u => u.region === region).length;
-      });
-const contenedor=document.getElementById("contenedor");
-contenedor.innerHTML="";
+    conteo[region] = data.filter(u => u.region === region).length;
+    });
+    const contenedor=document.getElementById("contenedor");
+    contenedor.innerHTML="";
 
-const tabla=document.createElement("table");
-table.border="1";
+    const tabla=document.createElement("table");
+    table.border="1";
 
-const thead = document.createElement('thead');
-thead.innerHTML = '<tr><th>Región</th><th>Cantidad</th></tr>';
-tabla.appendChild(thead);
+    const thead = document.createElement('thead');
+    thead.innerHTML = '<tr><th>Región</th><th>Cantidad</th></tr>';
+    tabla.appendChild(thead);
 
-  }
+    const tbody = document.createElement('tbody');
+    for (let region in conteo) {
+        const fila = document.createElement('tr');
+        fila.innerHTML = `<td>${region}</td><td>${conteo[region]}</td>`;
+        tbody.appendChild(fila);
+    }
+    tabla.appendChild(tbody);
+    contenedor.appendChild(tabla);
+    });
+}
