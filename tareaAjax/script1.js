ffab79717ae6783d9f61037f3ad9e3795c5f66d9
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-05-02 20:39:18 -0500

    funcion mostrar tabla, solo lee las regiones selecionadas y las filtra

--- tareaAjax/script1.js
+++ tareaAjax/script1.js
@@ -4,3 +4,14 @@ function getSeleccionRegiones() {
     checkboxes.forEach(cb => regiones.push(cb.value));
     return regiones;
   }
+  function mostrarTabla(){
+    fetch('/data')
+    .then(response => response.json())
+    .then(data => {
+    const regionSeleccionada = getSeleccionRegiones();
+    const conteo = {};
+
+    regionSeleccionada.forEach(region => {
+        conteo[region] = data.filter(u => u.region === region).length;
+      });
+  }
