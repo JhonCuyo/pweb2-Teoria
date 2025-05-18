53c132aae5bc7da122614a52630e4d81ca607e8f
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-05-17 21:31:51 -0500

    mostrando los datos de las peliculas siempre cuando es un arreglo, y mostrar los errores

--- tareaSqlite/script.js
+++ tareaSqlite/script.js
@@ -6,6 +6,19 @@ $(document).ready(function () {
             dataType: "json",
             success: function (data) {
                 $("#resultado").empty();
+                if (Array.isArray(data)) {
+                    data.forEach(function (p){
+                        $("#resultado").append(
+                            `<p><strong>${p.Title}</strong> (${p.Year}) - Score: ${p.Score}</p>`
+                        );
+                    });
+                } else {
+                    $("#resultado").append("<p>La respuesta no es un arreglo.</p>");
+                }
+            },
+            error: function (xhr, status, error) {
+                console.error("Error al obtener los datos:", status, error);
+                $("#resultado").html("<p>Error al obtener los datos.</p>")
             }
         });
     });
