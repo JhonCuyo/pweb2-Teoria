368965775a58b0ef2daa2c6daea8007cebb757bc
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-06-05 14:45:28 -0500

    agregando un nuevo metodo en views

--- templates_Django/listaContactos/personas/views.py
+++ templates_Django/listaContactos/personas/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from .models import Persona
+from .forms import PersonaForm
 
 def personaTestView(request):
     obj = Persona.objects.get(id=1)
     context = {
-        'nombre': obj.nombres,
-        'edad': obj.edad,
+        'objeto': obj,
         }
     return render(request, 'personas/descripcion.html', context)
     
+def personaCreativeView(request):
+    form = PersonaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = PersonaForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'personas/crear.html', context)
