7e167a3aa2875ba79349c7c722a0c9bed8f9b45a
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-06-05 15:46:07 -0500

    cambiando el metodo persona creative view

--- templates_Django/listaContactos/personas/views.py
+++ templates_Django/listaContactos/personas/views.py
@@ -10,15 +10,10 @@ def personaTestView(request):
     return render(request, 'personas/descripcion.html', context)
     
 def personaCreativeView(request):
-    form = PersonaForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = PersonaForm()
-
-    context = {
-        'form': form
-    }
-    return render(request, 'personas/crear.html', context)
+    print('GET:', request.GET)
+    print('POST:', request.POST)
+    context = {}
+    return render(request, 'personas/personasCreate.html', context)
 
 def searchForHelp(request):
     return render(request, 'personas/search.html', {})
