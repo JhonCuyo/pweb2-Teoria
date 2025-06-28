532fb4e21ff2a68d07b3a5ffee63ec32707a273c
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-06-28 18:54:44 -0500

    en vistas creando la clase PersonaListView

--- templates_Django/listaContactos/personas/views.py
+++ templates_Django/listaContactos/personas/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Persona
 from .forms import PersonaForm
+from django.views.generic.list import (ListView,)
 
 def personaTestView(request):
     obj = Persona.objects.get(id=1)
@@ -19,3 +20,5 @@ def personaCreativeView(request):
 
 def searchForHelp(request):
     return render(request, 'personas/search.html', {})
+class PersonaListView(ListView):
+    model= Persona
