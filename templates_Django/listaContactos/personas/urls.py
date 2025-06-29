cdbc7490c720f6117674d73814b926c703283ab1
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-06-28 19:49:04 -0500

    agregando la clase createview en urls

--- templates_Django/listaContactos/personas/urls.py
+++ templates_Django/listaContactos/personas/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import (PersonaListView, PersonaDetailView,)
+from .views import (PersonaListView, PersonaDetailView, PersonaCreateView)
 
 app_name = 'personas'
 urlpatterns = [
     path('', PersonaListView.as_view(), name='persona-list'),
     path('<int:pk>/', PersonaDetailView.as_view(), name='persona-detail'),
+    path('create/', PersonaCreateView.as_view(), name='persona-create'),
 ]
