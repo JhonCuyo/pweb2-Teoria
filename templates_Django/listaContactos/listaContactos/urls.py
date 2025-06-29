f835aad922699d681a424a0217ad719e0da2c98c
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-06-28 19:03:16 -0500

    agregando las urls creada en el proyecto

--- templates_Django/listaContactos/listaContactos/urls.py
+++ templates_Django/listaContactos/listaContactos/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from inicio.views import myHomeView
 from inicio.views import anotherView
 from personas.views import personaTestView, personaCreativeView, searchForHelp
@@ -27,4 +27,5 @@ urlpatterns = [
     path('search/', searchForHelp, name='buscar'),
     path('persona/', personaTestView, name='testViewPersona'),
     path('agregar/', personaCreativeView, name='createPersona'),
+    path('personas/', include('personas.urls') )
 ]
