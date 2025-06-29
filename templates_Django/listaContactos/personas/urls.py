45a2627d3b08cd4100c6301f9f5426bde705c213
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-06-28 19:58:03 -0500

    agregando la url del clase update

--- templates_Django/listaContactos/personas/urls.py
+++ templates_Django/listaContactos/personas/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import (PersonaListView, PersonaDetailView, PersonaCreateView)
+from .views import (PersonaListView, PersonaDetailView, PersonaCreateView, 
+                    PersonaUpdateView)
 
 app_name = 'personas'
 urlpatterns = [
     path('', PersonaListView.as_view(), name='persona-list'),
     path('<int:pk>/', PersonaDetailView.as_view(), name='persona-detail'),
     path('create/', PersonaCreateView.as_view(), name='persona-create'),
+    path('<int:pk>/update/', PersonaUpdateView.as_view(), name='persona-update'),
 ]
