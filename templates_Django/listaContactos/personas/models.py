6e83f442830b0bac70a5eeee203952c1a45afb98
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-06-28 19:36:10 -0500

    implementando el metodo get absolute url

--- templates_Django/listaContactos/personas/models.py
+++ templates_Django/listaContactos/personas/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Persona(models.Model):
@@ -6,3 +7,6 @@ class Persona(models.Model):
     apellidos = models.CharField(max_length=100)
     edad = models.IntegerField(blank = True)
     donador = models.BooleanField()
+
+    def get_absolute_url(self):
+        return reverse('personas:personadetail', kwargs={'pk': self.id})
